9fd78cf6ff8307e7d9ab317a6435a5a6237bfded
karthikprabhu001 <nnm22is067@nmamit.in>
2025-03-18 12:45:43 +0530

    Add files via upload

---
+++ diffie.py
@@ -0,0 +1,16 @@
+
+q = int(input("Enter a prime number : "))
+a = int(input("Enter a primitive root :"))
+Xa = int(input("Enter the private key of A :"))
+Xb = int(input("Enter the private key of B : "))
+Ya = a**Xa % q
+#Yb = math.pow(a, Xb) % q
+Yb = a**Xb % q
+print("Public key of A : ",Ya)
+print("Public key of B : ",Yb)
+#Ka = math.pow(Yb,Xa) % q
+Ka = Yb**Xa % q
+#Kb = math.pow(Ya,Xb) % q
+Kb = Ya**Xb % q
+print("Shared key for A : ",Ka)
+print("Shared key for B : ",Kb)
